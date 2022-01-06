59a554ce9870c9530d9da687c0b5dc843b7e4901
hgodgog <95087372+hgodgog@users.noreply.github.com>
2022-01-06 16:05:32 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World - code star',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
